08ade03f3c6055ff173e885bd532f730c4155840
JaHIY <jaklsy@gmail.com>
2023-11-24 15:39:40 +0800

    fix zombie processes problem

--- src/index.ts
+++ src/index.ts
@@ -88,7 +88,48 @@ async function setupWebDriver(): Promise<WebDriver> {
     const chromeOptions: Chrome.Options = new Chrome.Options();
     chromeOptions.headless();
     chromeOptions.setPageLoadStrategy('eager');
-    chromeOptions.addArguments('--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage');
+    chromeOptions.addArguments(
+        '--log-level=3', // fatal only
+        '--no-default-browser-check',
+        '--disable-site-isolation-trials',
+        '--no-experiments',
+        '--ignore-gpu-blacklist',
+        '--ignore-certificate-errors',
+        '--ignore-certificate-errors-spki-list',
+        '--disable-gpu',
+        '--disable-extensions',
+        '--disable-default-apps',
+        '--enable-features=NetworkService',
+        '--disable-setuid-sandbox',
+        '--no-sandbox',
+        // Extra
+        '--disable-webgl',
+        '--disable-infobars',
+        '--window-position=0,0',
+        '--ignore-certifcate-errors',
+        '--ignore-certifcate-errors-spki-list',
+        '--disable-threaded-animation',
+        '--disable-threaded-scrolling',
+        '--disable-in-process-stack-traces',
+        '--disable-histogram-customizer',
+        '--disable-gl-extensions',
+        '--disable-composited-antialiasing',
+        '--disable-session-crashed-bubble',
+        '--disable-canvas-aa',
+        '--disable-3d-apis',
+        '--disable-accelerated-2d-canvas',
+        '--disable-accelerated-jpeg-decoding',
+        '--disable-accelerated-mjpeg-decode',
+        '--disable-app-list-dismiss-on-blur',
+        '--disable-accelerated-video-decode',
+        '--disable-dev-shm-usage',
+        '--js-flags=--expose-gc',
+        '--disable-features=site-per-process',
+        '--disable-gl-drawing-for-tests',
+        '--disable-background-timer-throttling',
+        '--disable-backgrounding-occluded-windows',
+        '--disable-renderer-backgrounding',
+    );
 
     const driver: WebDriver = await new Builder()
         .forBrowser('chrome')
